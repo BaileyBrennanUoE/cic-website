1deaaa3a19c94b47fca2e95999aed5e2afff285a
BaileyBrennanUoE <bbrennan04@gmail.com>
2021-03-24 12:09:25 +0000

    Added more pages and embed vids

--- src/App.js
+++ src/App.js
@@ -7,40 +7,82 @@ import {
 } from 'react-router-dom';
 
 import Home from './components/Home';
-import CPU from './components/CPU'
+import CPU from './components/CPU';
+import Computer from './components/Computer';
+import Ram from './components/Ram';
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <nav>
-          <ul>
-            <li>
-              <NavLink 
-                exact to="/" 
-                activeClassName="selected">
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink 
-                to="/CPU" 
-                activeClassName="selected">
-                CPU
-              </NavLink>
-            </li>
-          </ul>
-        </nav>        <Switch>
-          <Route path="/CPU">
-            <CPU />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+import M from 'materialize-css';
+
+class App extends Component {
+  componentDidMount(){
+    let dropdowns = document.querySelectorAll(".dropdown-trigger");
+    let options = {
+      inDuration: 300,
+      outDuration: 225,
+      hover: true,
+      coverTrigger: false
+    };
+    M.Dropdown.init(dropdowns, options);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <nav>
+            <div className="nav-wrapper">
+              <ul id="nav-mobile" className="hide-on-med-and-down">
+                <li>
+                  <NavLink 
+                    exact to="/" 
+                    activeClassName="selected">
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink 
+                    to="/Computer" 
+                    activeClassName="selected">
+                    What's in a Computer
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink 
+                    to="/CPU" 
+                    activeClassName="selected">
+                    CPU
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink 
+                    to="/Ram" 
+                    activeClassName="selected">
+                    RAM vs Storage
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </nav>        
+          <Switch>
+            <Route path="/Computer">
+              <Computer />
+            </Route>
+            <Route path="/CPU">
+              <CPU />
+            </Route>
+            <Route path="/Ram">
+              <Ram />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
---
+++ src/components/Ram.jsx
@@ -0,0 +1,13 @@
+import React, { Component} from 'react';
+
+class Ram extends Component {
+    render() {
+        return (
+            <div className="Ram">
+                This is the ram page.
+            </div>
+        )
+    }
+}
+
+export default Ram;
